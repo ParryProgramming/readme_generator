b5af34f185038792bbbb7494e8044656a8582b6e
Parry <parryprogramming@gmail.com>
2024-04-24 02:15:46 -0400

    edited markdown license function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -18,11 +18,46 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== "none") {
+    return `## License
+    
+        Licensed under the ${license} license.`;
+  }
+  return "";
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  by ${data.name}
+  ${renderLicenseBadge(data.license)}
+  ## Table Of Contents
+  * [Description](#description)
+  * [Requirements](#requirements)
+  * [Usage](#Usage)
+  * [Contact-Me](#contact-me)
+  * [Contributors](#contributors)
+  * [Testing](#testing)
+  ${renderLicenseLink(data.license)}
+  ## Description
+  ${data.description}
+  ## Requirements
+  ${data.require}
+  ## Usage
+  ${data.usage}
+  ## Contact-Me
+  * Name - ${data.name}
+  * Email - ${data.email}
+  * Github - [${data.creator}](https://github.com/${data.creator}/)
+  ## Contributors
+  ${data.contributors}
+  ## Testing
+  \`\`\`
+  ${data.test}
+  \`\`\`
+  ${renderLicenseSection(data.license)}
+
 
 `;
 }
